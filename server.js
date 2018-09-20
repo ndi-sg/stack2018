b8279ada42efcf1d2693ada02a8d8c42cb6f751d
Codicem <lowyeewee@gmail.com>
2018-09-20 18:50:35 +0800

    add html to protected url

--- server.js
+++ server.js
@@ -10,17 +10,24 @@ var loki        = require('lokijs');
 var config      = require('./config'); // get our config file
 var app         = express();
 
+
 // =======================
 // in-memory db =========
 // =======================
 var db = new loki('db.json');
 var users = db.addCollection('users');
+var results = users.insert([
+    { name: 'Thor', password: 'password', admin: 'true'}, 
+    { name: 'Loki', password: 'password', admin: 'true'}
+]);
+
 
 // =======================
 // configuration =========
 // =======================
 var port = process.env.PORT || 3000; // used to create, sign, and verify tokens
-// mongoose.connect(config.database); // connect to database
+var path = __dirname + '/views/';
+app.use(express.static('assets'));
 app.set('superSecret', config.secret); // secret variable
 
 // use body parser so we can get info from POST and/or URL parameters
@@ -35,7 +42,9 @@ app.use(morgan('dev'));
 // =======================
 // basic route
 app.get('/', function(req, res) {
-    res.send(`Hello! The API is at http://localhost:${port}/api`);
+    // res.send(`Hello! The API is at http://localhost:${port}/api`);
+    res.sendFile(path + "index.html");
+
 });
 
 app.get('/setup', function(req, res) {
@@ -51,7 +60,7 @@ app.get('/setup', function(req, res) {
     } else {
         throw new Error("cannot insert into db");
     }
-  });
+});
 
 app.get('/setup/users', function(req, res) {
     var results = users.find({});
@@ -119,6 +128,16 @@ apiRoutes.use(function(req, res, next) {
     }
   });
 
+apiRoutes.get('/dashboard', function(req, res) {
+    res.sendFile(path + "dashboard.html");
+
+});
+
+apiRoutes.get('/orders', function(req, res) {
+    res.sendFile(path + "orders.html");
+
+});
+
 // route to show a random message (GET http://localhost:3000/api/)
 apiRoutes.get('/', function(req, res) {
   res.json({ message: 'Welcome to NDI Workshop!' });
@@ -132,6 +151,9 @@ apiRoutes.get('/users', function(req, res) {
 // apply the routes to our application with the prefix /api
 app.use('/api', apiRoutes);
 
+app.use("*",function(req,res){
+    res.sendFile(path + "404.html");
+});
 
 // =======================
 // start the server ======
