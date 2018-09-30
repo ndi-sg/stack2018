0b3ce6e22c79a05277bd713c942c882d15eee908
Codicem <lowyeewee@gmail.com>
2018-09-30 14:11:17 +0800

    add hss endpoint

--- config.js
+++ config.js
@@ -2,6 +2,7 @@ module.exports = {
 
     'secret': 'givememorendiworkshop',
     'ndi_asp_endpoint':'https://sandbox.api.ndi.gov.sg/asp/api/v1/asp',
+    'ndi_hss_endpoint':'https://sandbox.api.ndi.gov.sg/hss/api/v1/hss',
     'ndi_client_id':'clientlocal3000',
     'ndi_client_secret':'R3k37GqzavyjIKgslhzRRV0W'   
 
